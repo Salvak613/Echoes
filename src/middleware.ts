d52cf33a2989a815745ece620670e3f641dd2350
Salvak613 <jules.clauwaert@gmail.com>
2025-07-25 14:58:25 +0200

    fix for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 const publicPaths = ["/", "/echoes"];
+const ALLOWED = [
+  process.env.ALLOWED_ORIGIN_LOCAL!,
+  process.env.ALLOWED_ORIGIN_PROD!,
+];
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -16,7 +20,22 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
   }
 
+  const origin = request.headers.get("origin") || "";
+
   const res = NextResponse.next();
+
+  if (ALLOWED.includes(origin)) {
+    res.headers.set("Access-Control-Allow-Origin", origin);
+    res.headers.set(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PATCH,DELETE,OPTIONS"
+    );
+    res.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type,Authorization"
+    );
+    res.headers.set("Access-Control-Allow-Credentials", "true");
+  }
 
   const token = await getToken({
     req: request,
